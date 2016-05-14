25b3a7c78367a1f1e663e0c7d3c5fcaec46fd76d
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-05-13 22:24:03 -0400

    5/13/2016

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,34 @@
 var Sequelize = require('sequelize');
 
 var source = {
-	localhost: {
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-}
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    },
+    jawsDB: {
+        port: 3306,
+        host: 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'tdhmrn6me2l5230w',
+        password: 'wryeypxggzz336q5 ',
+        database: 'burgers_db'
+    }
 }
 
-var selectedSource = source.localhost;
+var selectedSource = source.jawsDB;
 
 
 var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
-	host: selectedSource.host,
-	dialect: 'mysql',
+    host: selectedSource.host,
+    dialect: 'mysql',
 
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	},
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
 });
 
 module.exports = sequelize;
